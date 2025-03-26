4c364b9e1d75fb93e3d8f350c25b4079df993da6
Namu <leevan.david@pit.fr>
2025-03-26 16:24:23 +0100

    feat: fillDatabase script

---
+++ mongo/databaseFill.ts
@@ -0,0 +1,15 @@
+import { InsertManyResult, MongoClient } from "mongodb";
+import * as MongoManager from "../mongoManager";
+import * as faker from "../faker/fakerHandler";
+
+
+export async function fillDatabase() {
+    const listUser : MongoUser[] = faker.generateUsers(100);
+    const collection = MongoManager.client.db("shard").collection("COLLEC_01");
+
+    await collection.deleteMany({});
+
+    const result: InsertManyResult = await collection.insertMany(listUser);
+
+    console.log(`${result.insertedCount} documents were inserted`);
+}
--- mongoManager.ts
+++ mongoManager.ts
@@ -1,18 +1,18 @@
 import { MongoClient } from 'mongodb';
 // import * as AddingMango from './addMongo';
 // import * as ReadingMango from './readMongo';
-// import * as EditMango from './editMongo';
+import * as FillDatabase from './mongo/databaseFill';
 
 const uri = "mongodb://localhost:27017";
 export const client = new MongoClient(uri);
 
-// export const add = AddingMango;
+export const fill = FillDatabase;
 // export const read = ReadingMango;
 // export const edit = EditMango
 
 export async function connect() {
     await client.connect();
-    await client.db("SEL").command({ ping: 1 });
+    await client.db("shard").command({ ping: 1 });
     console.log("Connected successfully to server");
 }
 
