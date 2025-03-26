f3e65de854323e7876437f133e7d4faaffa1f572
Namu <leevan.david@pit.fr>
2025-03-26 16:06:01 +0100

    feat: added mongoUser model

--- scripts/faker_use.ts
+++ scripts/faker_use.ts
@@ -1,16 +1,17 @@
 import { faker } from '@faker-js/faker';
 
-function generateCOLLEC_01(nbr) {
-let list_COLLEC :any[] = []; 
-let i=0;
-    for (i=0 ; i<nbr; i++) {  
-        const COLLEC_01 = {
+function generateUsers(quantity: number): MongoUser[] {
+    const userList: MongoUser[] = []; 
+
+    for (let i = 0; i<quantity; i++) {  
+        const user: MongoUser = {
             id: i+1,
             nom: faker.person.lastName(),
         }
-        list_COLLEC.push (COLLEC_01);
+
+        userList.push(user);
     }
-    return list_COLLEC;
+    return userList;
 }
 
-console.log(generateCOLLEC_01(100));
+console.log(generateUsers(100));
